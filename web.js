655eece31f45f33c5ede52017da3ad5ad3b8fa91
Qordoba <qordoba@gmx.com>
2013-07-18 08:06:48 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -4,8 +4,8 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var buffer = new Buffer();
-    response.send(fs.readFileSync("index.html").toString());
+    var buff = new Buffer(fs.readFileSync("index.html"));
+    response.send(buff.toString());
 });
 
 var port = process.env.PORT || 5000;
